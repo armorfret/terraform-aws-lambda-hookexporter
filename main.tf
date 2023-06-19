29908e561c0e839471ea2133b2768c20ab0d28ef
Les Aker <me@lesaker.org>
2023-06-19 09:55:02 -0400

    bump module version

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
 
 module "apigw" {
   source  = "armorfret/apigw-lambda/aws"
-  version = "0.6.7"
+  version = "0.6.8"
 
   source_bucket  = var.lambda_bucket
   source_version = var.lambda_version
