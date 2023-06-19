17591415c75b4ef30f7c9fc1981bc69b6fc106fc
Les Aker <me@lesaker.org>
2023-06-19 08:56:11 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+terraform-aws-lambda-hookexporter
+=========
+
+[![GitHub Workflow Status](https://img.shields.io/github/actions/workflow/status/armorfret/terraform-aws-lambda-hookexporter/build.yml?branch=main)](https://github.com/armorfret/terraform-aws-lambda-hookexporter/actions)
+[![MIT Licensed](https://img.shields.io/badge/license-MIT-green.svg)](https://tldrlegal.com/license/mit-license)
+
+This modules deploys [hook-exporter](https://github.com/akerl/hook-exporter)
+
+## Usage
+
+# License
+
+terraform-aws-lambda-hookexporter is released under the MIT License. See the bundled LICENSE file for details.
---
+++ main.tf
@@ -0,0 +1,75 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+module "apigw" {
+  source  = "armorfret/apigw-lambda/aws"
+  version = "0.6.6"
+
+  source_bucket  = var.lambda_bucket
+  source_version = var.lambda_version
+  function_name  = "hookexporter_${var.data_bucket}"
+
+  environment_variables = {
+    S3_BUCKET = var.config_bucket
+    S3_KEY    = "config.yaml"
+  }
+
+  access_policy_document = data.aws_iam_policy_document.lambda_perms.json
+
+  hostname = var.hostname
+
+  binary_media_types = [
+    "*/*",
+  ]
+}
+
+module "publish_user" {
+  source         = "armorfret/s3-publish/aws"
+  version        = "0.7.0"
+  logging_bucket = var.logging_bucket
+  publish_bucket = var.data_bucket
+}
+
+module "config_user" {
+  source         = "armorfret/s3-publish/aws"
+  version        = "0.7.0"
+  logging_bucket = var.logging_bucket
+  publish_bucket = var.config_bucket
+  count          = var.config_bucket == var.data_bucket ? 0 : 1
+}
+
+data "aws_iam_policy_document" "lambda_perms" {
+  statement {
+    actions = [
+      "s3:ListBucket",
+      "s3:GetObject",
+      "s3:PutObject",
+    ]
+
+    resources = distinct([
+      "arn:aws:s3:::${var.data_bucket}/*",
+      "arn:aws:s3:::${var.data_bucket}",
+      "arn:aws:s3:::${var.config_bucket}/*",
+      "arn:aws:s3:::${var.config_bucket}",
+    ])
+  }
+
+  statement {
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+    ]
+
+    resources = [
+      "arn:aws:logs:*:*:*",
+    ]
+  }
+}
+
